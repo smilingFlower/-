679462ae7c4db33856f906f3f8c9927386af52b6
wintercherry <wuxiaohui@foreveross.com>
2017-02-15 17:48:12 +0800

    donghua

--- js/swipePhoto.js
+++ js/swipePhoto.js
@@ -89,130 +89,193 @@ var framework = {
 var swipePhoto = function(gallerySelector,items,options){
 
     var self = this,
-        direction = "horizontal";
+        direction = "horizontal",
+        outer = framework.getChildByClass(gallerySelector,"swiper-wrapper"), 
+        winW = window.innerWidth,
+        items = items,
+        list = [],
+        currentIndex = 0,
+        target,
+        startX,
+        startY,
+        moveEndX,
+        moveEndY,
+        X, //移动x的距离
+        Y,  //移动y的距离
+        startTime,
+        endTime,
+        direction;
 
-    //设置图片的样式
-    var setImageSize = function(target, maxRes) {
+    var updateIndex = function(){
+        
+    }
 
-    };
+    var animation = function(){
 
-    // //修改图片路径
-    // var modifyImageSrc = function(imgTarget,item){
-    //     console.log(imgTarget);
-    //     imgTarget.setAttribute("src",item);
-    // };  
+    }
 
+    var touchstartFun = function(e){           
+        console.log(e.target);            
+        console.log("touchstart");
+        e.preventDefault();
 
-    var slider = function(gallerySelector,items){
-        var traget = gallerySelector, //滑动的元素
-            items = items,
-            currentIndex = 0,
-            startX,
-            startY,
-            moveEndX,
-            moveEndY,
-            X, //移动x的距离
-            Y,  //移动y的距离
-            thouchDirection;
+        startX = e.targetTouches[0].pageX;
+        startY = e.targetTouches[0].pageY;
 
-        var updateProgress = function(translate){
-            if (typeof translate === "undefined") {
-                return;
-            }
+        var target = e.target;
+        target = target;
 
+    }
 
+    var touchmoveFun = function(e){
+        console.log(e.target);
+        console.log("touchmove");
+        e.preventDefault();
+
+        startTime = new Date() * 1;  //把时间转成ms
+
+        moveEndX = e.targetTouches[0].pageX;
+        moveEndY = e.targetTouches[0].pageY;
+
+        X = Math.floor(moveEndX - startX);
+        Y = Math.floor(moveEndY - startY);
+
+        var moveDistanceY = Y < 0 ? -Y : Y;
+
+        if (moveDistanceY > 50) {
+            return false;
         }
 
-        var setWrapperTranslate = function(translate){
-            if (typeof translate === "undefined") {
-                return;
-            }
-
-
-        }
-
-        var touchstartFun = function(e){
-            console.log("touchstart");
-            e.preventDefault();
-
-            // e.targetTouches[0].pageX
-            startX = e.targetTouches[0].pageX;
-            startY = e.targetTouches[0].pageY;
-        }
-
-        var touchmoveFun = function(e){
-            console.log("touchmove");
-            e.preventDefault();
-
-            moveEndX = e.targetTouches[0].pageX;
-            moveEndY = e.targetTouches[0].pageY;
-
-            X = moveEndX - startX;
-            Y = moveEndY - startY;
-
-            var moveDistanceY = Y < 0 ? -Y : Y;
-
-            if (moveDistanceY > 50) {
-                return false;
-            }
-
-            if (direction === "horizontal") {
-                if  ( X > 5 ) {
-                    console.log("向右滑动");
-                }else if( X < -5 ){
-                    console.log("向左滑动");
+        if (direction === "horizontal") {
+            console.log("horizontal");
+            if  ( X > 5 ) {
+                console.log("向右滑动");
+                if (currentIndex == 0) { //滑动到第一个元素，不再滑动
+                    return false
                 }
-            }            
-        }
-
-        var touchendFun = function(e){
-            console.log("touchend");
-            e.preventDefault();
-
-        }
-
-        var bindEvent = function(){
-            // framework.bind(traget,'click',function(e){
-            //     console.log(e.pageX);
-            //     console.log(e.pageY);
-            // });
-            framework.bind(traget,'touchstart',touchstartFun);
-            framework.bind(traget,'touchmove',touchmoveFun);
-            framework.bind(traget,'touchend',touchendFun);           
-        }
-
-        var addhtml = function(){
-            var parent = framework.getChildByClass(traget,"swiper-wrapper");
-            var html = "";
-            for(var i = 0; i< items.length; i++){
-                html += '<div class="swiper-slide"> <img src="'+ items[i] +'"> </div>';
+                list[currentIndex].style.webkitTransform = "translate3d(" + (winW - X) +"px, 0, 0)";
+                list[currentIndex].style.transform = "translate3d(" + (winW - X) +"px, 0, 0)";
+                list[currentIndex-1].style.webkitTransform = "translate3d(" + X +"px, 0, 0)";
+                list[currentIndex-1].style.transform = "translate3d(" + X +"px, 0, 0)";  
+                direction = "right";
+            }else if( X < (-5) ){
+                console.log("向左滑动");
+                if (currentIndex == (list.length - 1)) { //滑动到第最后一个元素，不再滑动
+                    return false
+                }
+                list[currentIndex].style.webkitTransform = "translate3d(" + X +"px, 0, 0)";
+                list[currentIndex].style.transform = "translate3d(" + X +"px, 0, 0)";
+                list[currentIndex+1].style.webkitTransform = "translate3d(" + (winW + X) +"px, 0, 0)";
+                list[currentIndex+1].style.transform = "translate3d(" + (winW + X) +"px, 0, 0)";
+                direction = "left";
             }
-            parent.innerHTML = html;
+
+        }            
+    }
+
+    var touchendFun = function(e){
+        console.log(e.target);            
+        console.log("touchend");
+        e.preventDefault();
+
+        endTime = new Date() * 1;  //把时间转成ms
+
+        var time = 0; //动画的时间
+
+        if (direction === "right") {
+            // if (currentIndex == 0) {
+            //     return false;
+            // }
+
+            // if ( X < (winW / 3 * 2)) {
+            //     time = 1;
+            // }else{
+            //     time = .5;
+            // }
+
+            // //改变动画后所应该的位移值
+            // list[currentIndex].style.webkitTransform = 'translate3d('+ winW +'px, 0, 0)';
+            // list[currentIndex-1].style.webkitTransform = 'translate3d(0, 0, 0)';    
+
+            // list[currentIndex].style.webkitTransition = '-webkit-transform '+ time +'s ease-out';              
+            // list[currentIndex-1].style.webkitTransition = '-webkit-transform '+ time +'s ease-out'; 
+
+
+            // currentIndex--;
+        }else{
+            // if (currentIndex == (list.length - 1)) {
+            //     return false;
+            // }
+
+            // if ( -X < (winW / 3 * 2)) {
+            //     time = 1;
+            // }else{
+            //     time = .5;
+            // }  
+
+            // //改变动画后所应该的位移值
+            // list[currentIndex].style.webkitTransform = 'translate3d('+ (-winW) +'px, 0, 0)';
+            // list[currentIndex+1].style.webkitTransform = 'translate3d(0, 0, 0)';    
+
+            // list[currentIndex].style.webkitTransition = '-webkit-transform '+ time +'s ease-out';              
+            // list[currentIndex+1].style.webkitTransition = '-webkit-transform '+ time +'s ease-out';  
+            
+            // currentIndex++;
+        }
+    }
+
+    var bindEvent = function(){
+        // framework.bind(target,'click',function(e){
+        //     console.log(e.pageX);
+        //     console.log(e.pageY);
+        // });
+        framework.bind(outer,'touchstart',touchstartFun);
+        framework.bind(outer,'touchmove',touchmoveFun);
+        framework.bind(outer,'touchend',touchendFun);           
+    }
+
+    var addhtml = function(){
+        var parent = framework.getChildByClass(gallerySelector,"swiper-wrapper");
+        var html = "";
+        for(var i = 0; i< items.length; i++){
+            html += '<div class="swiper-slide" data='+i+'> <img src="'+ items[i] +'"></div>';
+        }
+        parent.innerHTML = html;
+    }
+
+    var unbindEvent = function(){
+        framework.unbind(outer,'touchstart',touchstartFun);
+        framework.unbind(outer,'touchmove',touchmoveFun);
+        framework.unbind(outer,'touchend',touchendFun);           
+    }
+
+    var init = function(){
+        $(gallerySelector).removeClass("hidden");
+        var overlay = document.getElementsByClassName("overlay")[0];
+        framework.removeClass(overlay,"hidden");
+
+        addhtml();
+
+        var item = framework.getChildByClass(gallerySelector,"swiper-wrapper").childNodes;  
+        for (var i = 0; i < item.length; i++) {
+            if (framework.hasClass(item[i],"swiper-slide")) {
+                list.push(item[i]);
+                item[i].style.webkitTransform = "translate3d(" + i * winW +"px, 0, 0)";
+                item[i].style.transform = "translate3d(" + i * winW +"px, 0, 0)";
+            }
         }
 
-        var unbindEvent = function(){
-            framework.unbind(traget,'touchstart',touchstartFun);
-            framework.unbind(traget,'touchmove',touchmoveFun);
-            framework.unbind(traget,'touchend',touchendFun);           
-        }
-
-        var init = function(){
-            addhtml();
-            bindEvent();
-        }
-
-        init();
-
+        bindEvent();
     }
 
     var publicMethods = {
         options:options,
         init:function(){
-            slider(gallerySelector,items);
+            init();
         }
     }
 
-    framework.extend(self, publicMethods)
+    framework.extend(self, publicMethods); 
 }
 
 
